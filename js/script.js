02bedd5baa1ee726e45bf8cb1fed8e89707b4903
ymarfoq <marfoq.yohan@gmail.com>
2015-11-20 17:21:15 -0500

    Ajout de l'album photo, non relié à la base

--- js/script.js
+++ js/script.js
@@ -32,8 +32,6 @@ $(document).ready(function (e) {
 		return false;
 	}));
 
-
-
 	// Function to preview image after validation
 	$(function() {
 		$("#photo").change(function() {
@@ -58,5 +56,40 @@ $(document).ready(function (e) {
 		$('#load_photo').attr('src', e.target.result);
 		$('#load_photo').attr('height', '50px');
 	};
+           
+	var jssor_1_SlideshowTransitions = [{$Duration:3000,$Opacity:2}];            
+	var jssor_1_options = {
+		$AutoPlay: true,
+		$SlideshowOptions: {
+			$Class: $JssorSlideshowRunner$,
+			$Transitions: jssor_1_SlideshowTransitions,
+			$TransitionsOrder: 1
+		},
+		$ArrowNavigatorOptions: {
+			$Class: $JssorArrowNavigator$
+		},
+		$BulletNavigatorOptions: {
+			$Class: $JssorBulletNavigator$
+		}
+	};
+            
+	var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+            
+	//responsive code begin
+	//you can remove responsive code if you don't want the slider scales while window resizes
+	function ScaleSlider() {
+		var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+		if (refSize) {
+			refSize = Math.min(refSize, 4000);
+			jssor_1_slider.$ScaleWidth(refSize);
+		}
+		else {
+			window.setTimeout(ScaleSlider, 30);
+		}
+	}
+	ScaleSlider();
+	$(window).bind("load", ScaleSlider);
+	$(window).bind("resize", ScaleSlider);
+	$(window).bind("orientationchange", ScaleSlider);
+	//responsive code end
 });
-
